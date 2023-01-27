3ae47f7480fc641a1771e4354c46ea175b7dc3bb
Istvan Zilahi <istvan.zilahi98@gmail.com>
2023-01-27 17:48:42 +0100

    Slider background changer has been added.

--- psw_generator/src/comps/input/Slider/Slider.js
+++ psw_generator/src/comps/input/Slider/Slider.js
@@ -5,6 +5,9 @@ import "./Slider.scss";
 const Slider = (props) => {
   const { slider, setSlider, setIcon } = props;
 
+  const sliderMinVal = 0;
+  const sliderMaxVal = 20;
+
   const handleSlider = (event) => {
     setSlider(event.target.value);
     setIcon(false);
@@ -15,11 +18,16 @@ const Slider = (props) => {
       <input
         className="slider-item"
         type="range"
-        min="0"
-        max="20"
+        min={sliderMinVal}
+        max={sliderMaxVal}
         step="1"
         onChange={handleSlider}
         value={slider}
+        style={{
+          backgroundImage: `linear-gradient(to right, #a4ffaf ${
+            (slider / (sliderMaxVal - sliderMinVal)) * 100
+          }%, #100f15 0%)`,
+        }}
       />
     </div>
   );
